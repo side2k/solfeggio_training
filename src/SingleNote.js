638411fda18353d26d6cadf8b4f0208a924b61c2
Leonid Amirov <leonid.amirov@gmail.com>
2022-04-08 20:23:27 +0300

    feat: generate random note

--- src/SingleNote.js
+++ src/SingleNote.js
@@ -1,14 +1,33 @@
 import { useRef, useEffect } from "react";
 import { renderAbc } from "abcjs";
 
-var demoAbc = "T: Какая это нота?\n" + "L: 1/4\n" + "K: clef=treble\n" + "c";
+function generateRandomNote() {
+  const NOTES = ["C", "D", "E", "F", "G", "A", "B"];
+  const randomNote = NOTES[Math.floor(Math.random() * NOTES.length)];
+
+  return randomNote;
+}
+
+function generateNotationWithRandomNote() {
+  const note = generateRandomNote();
+  return {
+    note: note,
+    octave: 4,
+    notation: `
+    T: Какая это нота?
+    L: 1/4
+    K: clef=treble
+    ${note}`,
+  };
+}
 
 const SingleNote = () => {
   const abcRef = useRef();
 
   useEffect(() => {
+    const { note, octave, notation } = generateNotationWithRandomNote();
     console.log(abcRef.current);
-    renderAbc(abcRef.current.id, demoAbc);
+    renderAbc(abcRef.current.id, notation);
   }, []);
   return (
     <div>
