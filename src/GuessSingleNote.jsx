257212b8f059248d8c73557825100be12584e789
Leonid Amirov <leonid.amirov@gmail.com>
2022-04-10 17:18:59 +0300

    feat: play sounds on right/wrong answer

--- src/GuessSingleNote.jsx
+++ src/GuessSingleNote.jsx
@@ -17,17 +17,22 @@ const GuessSingleNote = () => {
   const [alternator, setAlternator] = useState(false);
   const { note, octave } = useMemo(() => generateRandomNote(), [alternator]);
   const noteSelector = useRef();
-  const indicatorTimeout = 1500;
+  const indicatorTimeout = 500;
   const [rightAnswersCount, setRightAnswersCount] = useState(0);
   const [wrongAnswersCount, setWrongAnswersCount] = useState(0);
+
+  const beepRight = new Audio("./beep_right.mp3");
+  const beepWrong = new Audio("./beep_wrong.mp3");
 
   const onSelectNote = ({ selectedNote, selectedOctave }) => {
     if (selectedNote === note && selectedOctave === octave) {
       noteSelector.current.indicateRight(indicatorTimeout);
       setRightAnswersCount(rightAnswersCount + 1);
+      beepRight.play();
     } else {
       noteSelector.current.indicateWrong(indicatorTimeout);
       setWrongAnswersCount(wrongAnswersCount + 1);
+      beepWrong.play();
     }
     setTimeout(() => {
       setAlternator(!alternator);
