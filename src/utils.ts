c94c163ff5e6923563bc38ee5dc5231147d53458
Leonid Amirov <leonid.amirov@gmail.com>
2022-04-16 11:32:06 +0300

    refactor(utils): simplify functions and remove obsolete code

--- src/utils.ts
+++ src/utils.ts
@@ -1,39 +1,25 @@
-import { Note, Octave } from "./types";
+import { Note, Octave, NoteData } from "./types";
+import {
+  NOTE_NAMES,
+  OCTAVE_LABELS,
+  OCTAVE_SHORT_LABELS,
+} from "./languages/russian";
 
-export const NOTES = ["C", "D", "E", "F", "G", "A", "B"];
-export const NOTE_NAMES = ["До", "Ре", "Ми", "Фа", "Соль", "Ля", "Си"];
-export const OCTAVES = [0, 1, 2, 3, 4, 5, 6];
-export const OCTAVE_LABELS = [
-  "суб-контр", // 0
-  "контр", // 1
-  "большой", // 2
-  "малой", // 3
-  "первой", // 4
-  "второй", // 5
-  "третьей", //6
-];
-
-export const OCTAVE_SHORT_LABELS = [
-  "СК", // 0
-  "К", // 1
-  "Б", // 2
-  "М", // 3
-  "1", // 4
-  "2", // 5
-  "3", //6
-];
-
-export function noteToName(note: Note, octave: Octave) {
-  const noteLabelIndex = NOTES.indexOf(note);
-  const octaveLabelIndex = OCTAVES.indexOf(octave);
-  console.log(note, octave, octaveLabelIndex);
-
-  return `${NOTE_NAMES[noteLabelIndex]} ${OCTAVE_LABELS[octaveLabelIndex]} октавы`;
+export function noteToName(note: Note, octave: Octave): string {
+  return `${NOTE_NAMES[note]} ${OCTAVE_LABELS[octave]} октавы`;
 }
 
-export function noteToShortName(note: Note, octave: Octave) {
-  const noteLabelIndex = NOTES.indexOf(note);
-  const octaveLabelIndex = OCTAVES.indexOf(octave);
+export function noteToShortName({ note, octave }: NoteData): string {
+  return `${NOTE_NAMES[note]}-${OCTAVE_SHORT_LABELS[octave]}`;
+}
 
-  return `${NOTE_NAMES[noteLabelIndex]}-${OCTAVE_SHORT_LABELS[octaveLabelIndex]}`;
+export function generateRandomNote(): NoteData {
+  const notes = Object.values(Note);
+
+  const note = notes[Math.floor(Math.random() * notes.length)];
+
+  const availableOctaves: Octave[] = [Octave.Line1, Octave.Line2];
+  const octave =
+    availableOctaves[Math.floor(Math.random() * availableOctaves.length)];
+  return { note, octave };
 }
